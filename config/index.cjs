80aab03f622d78971e9deccd843f53ef55196263
Mr.Y <107235288+doll1003cat@users.noreply.github.com>
2022-12-01 00:52:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '火火！',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oF7YI6DWOcMkbNd9PeKzbA7Iv5Lc',
+      id: 'oF7YI6F2DpzcYl8cZaSKW2J1ysow',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ENfSHaENMFy5xwp3LLEfxeBKB-hdFtsVyzznxdVNQY8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -42,13 +42,11 @@ const USER_CONFIG = {
         { keyword: 'love_day', date: '2022-02-13' },//
       ],
     },
-  ],
-  USERS: [
     {
       // 想要发送的人的名字
       name: '火火！',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oF7YI6F2DpzcYl8cZaSKW2J1ysow',
+      id: 'oF7YI6DWOcMkbNd9PeKzbA7Iv5Lc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ENfSHaENMFy5xwp3LLEfxeBKB-hdFtsVyzznxdVNQY8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
