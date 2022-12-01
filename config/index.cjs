e90780bd176e6e941548151017b7576333111956
Mr.Y <107235288+doll1003cat@users.noreply.github.com>
2022-12-02 00:33:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -32,9 +32,6 @@ const USER_CONFIG = {
         {
           type: '生日', name: '火火', year: '2006', date: '05-05',
         },
-        {
-          type: '节日', name: '相识纪念日', year: '2022', date: '02-13',//实际上是12号呜呜呜
-        },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
@@ -56,9 +53,6 @@ const USER_CONFIG = {
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '火火', year: '2006', date: '05-05',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2022', date: '02-13',//实际上是12号呜呜呜
         },
       ],
       // 我们在一起已经有xxxx天了的配置
